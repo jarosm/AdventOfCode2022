7145af114712d14706c127d447ab76d97ac12288
Martin Jaros <jarosm@centrum.cz>
2022-11-28 16:47:37 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+Part1();
+
+Part2();
+
+Console.WriteLine("end");
+Console.ReadLine();
+
+
+string[] ParseInput()
+{
+	var input = File.ReadAllLines("input.txt");
+
+	return input;
+}
+
+
+void Part1()
+{
+	var input = ParseInput();
+}
+
+
+void Part2()
+{
+	var input = ParseInput();
+}
